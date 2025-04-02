33e105aef80b28ddf28fa2d18a30ff039cb85a34
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2025-04-02 12:43:42 +0530

    djhjhjhjdh

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,7 +1,7 @@
 const admin = require("firebase-admin");  // Use "admin" instead of "firebase"
 
-const serviceAccount = require("./service-acount-key.json");
-
+// const serviceAccount = require("./service-acount-key.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
