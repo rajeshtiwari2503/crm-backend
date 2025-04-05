6e670c8ecbd181eaa7048e5f8c223c601112d9b8
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2025-04-05 14:10:22 +0530

    djjjhhjd

--- src/googleConfig/uploadMiddleware.js
+++ src/googleConfig/uploadMiddleware.js
@@ -1,6 +1,7 @@
 const multer = require("multer");
 
 // Configure Multer storage
-const uploadVideo = multer({ dest: "uploads/" });
+// const uploadVideo = multer({ dest: "uploads/" });
+const uploadVideo = multer({ storage: multer.memoryStorage() });
 
 module.exports = uploadVideo;
